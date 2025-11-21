59cbe1c9f3ecbda7732bead448ce9e7250d1d618
Artzcode <artzcode.true@gmail.com>
2025-11-21 12:35:17 -0300

    fix Hero component UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -18,10 +18,18 @@ const Hero = () => {
         height: "100vh",
         display: "flex",
         alignItems: "center",
+        [theme.breakpoints.up('xs')]: { // <= mobile
+            paddingTop:"100px"
+        },
+
+        [theme.breakpoints.up('md')]: { // >= mobile
+            paddingTop:"0px"
+        }
+
     }))
 
     const Styledimg = styled("img")(({theme}) => ({
-        width: "80%",
+        width: "75%",
         borderRadius: "50%",
         border: `1px solid ${theme.palette.primary.contrastText}`,
     }));
